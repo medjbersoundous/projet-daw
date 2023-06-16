17630b620d0432cf632a697b8b5ef0e25d1ce375
sido200 <93400204+sido200@users.noreply.github.com>
2023-06-16 14:14:01 +0100

    sidali

--- voyage/ville.php
+++ voyage/ville.php
@@ -1,47 +1,140 @@
 <?php
 include("config/connect.php");
 
-
 if (isset($_GET['id'])) {
-    $id = mysqli_real_escape_string($conn,$_GET['id']);
+    $id = mysqli_real_escape_string($conn, $_GET['id']);
 }
-$sql = "SELECT ville.*, pays.nompays AS nom_pays, sites.*, necessaire.typenec, necessaire.nomnec
+$sql = "SELECT ville.*, pays.nompays AS nom_pays,  necessaire.typenec, necessaire.nomnec
         FROM ville
         JOIN pays ON ville.idpays = pays.idpays
-        JOIN sites ON ville.idville = sites.idville
         JOIN necessaire ON ville.idville = necessaire.idville
         WHERE ville.idville = $id";
+$sql2 = "SELECT necessaire.typenec, necessaire.nomnec
+FROM necessaire
+WHERE necessaire.idville = $id AND necessaire.typenec = 'restaurant'";
+$sql4 = "SELECT necessaire.typenec, necessaire.nomnec
+FROM necessaire
+WHERE necessaire.idville = $id AND necessaire.typenec = 'hotel'";
+$sql5 = "SELECT necessaire.typenec, necessaire.nomnec
+FROM necessaire
+WHERE necessaire.idville = $id AND (necessaire.typenec = 'gare' or necessaire.typenec = 'aeroport') ";
+$sql3 = "SELECT sites.*
+FROM sites
+WHERE sites.idville = $id ";
 
+$rslt2 = mysqli_query($conn, $sql2);
+$resto = array();
 
+while ($row = mysqli_fetch_assoc($rslt2)) {
+    $resto[] = $row;
+}
 
-$rslt=mysqli_query($conn,$sql);
-$villes=mysqli_fetch_assoc($rslt);
+$rslt4 = mysqli_query($conn, $sql4);
+$hotels = array();
+
+while ($row = mysqli_fetch_assoc($rslt4)) {
+    $hotels[] = $row;
+}
+$rslt5 = mysqli_query($conn, $sql5);
+$Transport = array();
+
+while ($row = mysqli_fetch_assoc($rslt5)) {
+    $Transport[] = $row;
+}
+
+$rslt3 = mysqli_query($conn, $sql3);
+$sites = array();
+
+while ($row = mysqli_fetch_assoc($rslt3)) {
+    $sites[] = $row;
+}
+
+$rslt = mysqli_query($conn, $sql);
+$villes = mysqli_fetch_assoc($rslt);
+
 mysqli_free_result($rslt);
+mysqli_free_result($rslt2);
+mysqli_free_result($rslt3);
 mysqli_close($conn);
-print_r($villes)
-
-
-
 ?>
+
 <!DOCTYPE html>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>projet daw</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+    <link rel="stylesheet" href="viLle.css">
+</head>
 <html>
-
-<h4 class='center'>Details</h4>
-<div class="container center">
-    <?php if ($villes): ?>
-        <h2><?php echo htmlspecialchars($villes['descville']); ?></h2>
-        <h4><?php echo htmlspecialchars($villes['nomville']); ?></h4>
-        <p>pays: <?php echo htmlspecialchars($villes['nom_pays']); ?></p>
-
-        <h5>site</h5>
-        <p><?php echo htmlspecialchars($villes['nomsite']); ?></p>
-        <?php if (!empty($villes['cheminphoto'])): ?>
-            <img src="<?php echo htmlspecialchars($villes['cheminphoto']); ?>" alt="Image de la ville">
-        <?php endif; ?>
-    <?php else: ?>
-        <h5>no villes</h5>
-        <?php echo print_r($villes); ?>
-    <?php endif; ?>
-</div>
-
+<?php if ($villes): ?>
+    <div class="ville">
+        <h2><?php echo htmlspecialchars($villes['nomville']); ?></h2>
+        <div class="bottom"></div>
+        <p>
+            <?php echo htmlspecialchars($villes['descville']); ?>
+        </p>
+        <h3>Top site</h3>
+        <div class="bottom2"></div>
+        <div class="grid">
+            <?php
+            foreach ($sites as $site) {
+                echo '<div class="sights">';
+                echo '<img src="' . htmlspecialchars($site['cheminphoto']) . '" alt="Image de la ville">';
+                echo '<p>' . htmlspecialchars($site['nomsite']) . '</p>';
+                echo '</div>';
+            }
+            ?>
+        </div>
+        <div class="grid">
+            <div class="flex">
+                <h3>Top resto</h3>
+                <div class="bottom2"></div>
+                <div class="sights2">
+                    <ul>
+                        <?php
+                        foreach ($resto as $rest) {
+                            echo '<li>' . htmlspecialchars($rest['nomnec']) . '</li>';
+                        }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+            <?php
+            if (!empty($hotels)) {
+                echo ' <div class="flex">';
+                echo '<h3>top hotel </h3>';
+                echo '<div class="bottom2"></div>';
+                echo '<div class="sights2">';
+                echo '<ul class="ul">';
+                foreach ($hotels as $hotel) {
+                    echo '<li>' . htmlspecialchars($hotel['nomnec']) . '</li>';
+                }
+                echo '</ul>';
+                echo '</div>';
+                echo '</div>';
+            }
+            ?>
+            <?php
+            if (!empty($Transport)) {
+                echo ' <div class="flex">';
+                echo '<h3>Transport </h3>';
+                echo '<div class="bottom2"></div>';
+                echo '<div class="sights2">';
+                echo '<ul class="ul">';
+                foreach ($Transport as $Transport) {
+                    echo '<li>' . htmlspecialchars($Transport['nomnec']) . '</li>';
+                }
+                echo '</ul>';
+                echo '</div>';
+                echo '</div>';
+            }
+            ?>
+      
+    </div>
+<?php else: ?>
+    <h5>no villes</h5>
+    <?php echo "No data available for villes."; ?>
+<?php endif; ?>
 </html>
